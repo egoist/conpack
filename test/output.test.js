52b3f8174a5fca42f1e0c7bef173c58d3b4c8ad5
EGOIST <0x142857@gmail.com>
2017-02-24 14:12:29 +0800

    improve test coverage for output

--- test/output.test.js
+++ test/output.test.js
@@ -14,6 +14,46 @@ describe('output', () => {
     })
   })
 
+  it('set filename', () => {
+    const config = new Config()
+
+    config
+      .output
+        .filename('[name].js')
+
+    expect(config.toConfig().output).toEqual({filename: '[name].js'})
+  })
+
+  it('set publicPath', () => {
+    const config = new Config()
+
+    config
+      .output
+        .publicPath(__dirname)
+
+    expect(config.toConfig().output).toEqual({publicPath: __dirname})
+  })
+
+  it('set library', () => {
+    const config = new Config()
+
+    config
+      .output
+        .library('foo')
+
+    expect(config.toConfig().output).toEqual({library: 'foo'})
+  })
+
+  it('set libraryTarget', () => {
+    const config = new Config()
+
+    config
+      .output
+        .libraryTarget('foo')
+
+    expect(config.toConfig().output).toEqual({libraryTarget: 'foo'})
+  })
+
   it('set custom property', () => {
     const config = new Config()
 
